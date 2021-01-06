5b472f763a6bd6456fb4e499dc0302c439483fc4
kangwoosukeq <kangwoosuk1@gmail.com>
2021-01-06 14:09:13 +0900

    init

---
+++ modules/gui/macosx/misc.h
@@ -0,0 +1,119 @@
+/*****************************************************************************
+ * misc.h: code not specific to vlc
+ *****************************************************************************
+ * Copyright (C) 2003 the VideoLAN team
+ * $Id: 38fb496b4d7daf183e1d2caf06eecb9c0703d309 $
+ *
+ * Authors: Jon Lech Johansen <jon-vl@nanocrew.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+/*****************************************************************************
+ * NSScreen (VLCAdditions)
+ *
+ *  Missing extension to NSScreen
+ *****************************************************************************/
+
+@interface NSScreen (VLCAdditions)
+
++ (NSScreen *)screenWithDisplayID: (CGDirectDisplayID)displayID;
+- (BOOL)isMainScreen;
+- (BOOL)isScreen: (NSScreen*)screen;
+- (CGDirectDisplayID)displayID;
+- (void)blackoutOtherScreens;
++ (void)unblackoutScreens;
+@end
+
+/*****************************************************************************
+ * VLCWindow
+ *
+ *  Missing extension to NSWindow
+ *****************************************************************************/
+
+@interface VLCWindow : NSWindow
+{
+    BOOL b_canBecomeKeyWindow;
+    BOOL b_isset_canBecomeKeyWindow;
+}
+
+- (void)setCanBecomeKeyWindow: (BOOL)canBecomeKey;
+@end
+
+/*****************************************************************************
+ * VLCControllerWindow
+ *****************************************************************************/
+
+
+@interface VLCControllerWindow : NSWindow
+{
+}
+
+@end
+
+/*****************************************************************************
+ * VLCControllerView
+ *****************************************************************************/
+
+@interface VLCControllerView : NSView
+{
+}
+
+@end
+
+/*****************************************************************************
+ * VLBrushedMetalImageView
+ *****************************************************************************/
+
+@interface VLBrushedMetalImageView : NSImageView
+{
+
+}
+
+@end
+
+
+/*****************************************************************************
+ * MPSlider
+ *****************************************************************************/
+
+@interface MPSlider : NSSlider
+{
+}
+
+@end
+
+/*****************************************************************************
+ * ITSliderCell
+ *****************************************************************************/
+ 
+@interface ITSlider : NSSlider
+{
+}
+
+@end
+
+/*****************************************************************************
+ * ITSliderCell
+ *****************************************************************************/
+ 
+@interface ITSliderCell : NSSliderCell
+{
+    NSImage *_knobOff;
+    NSImage *_knobOn;
+    BOOL b_mouse_down;
+}
+
+@end
---
+++ modules/gui/skins2/commands/cmd_add_item.cpp
@@ -0,0 +1,49 @@
+/*****************************************************************************
+ * cmd_add_item.cpp
+ *****************************************************************************
+ * Copyright (C) 2003 the VideoLAN team
+ * $Id: 9bf80b6744e40cee7bc4c98588fb351cb8d48ff3 $
+ *
+ * Authors: Cyril Deguet     <asmax@via.ecp.fr>
+ *          Olivier Teulière <ipkiss@via.ecp.fr>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+#include <vlc/vlc.h>
+#include "cmd_add_item.hpp"
+
+
+void CmdAddItem::execute()
+{
+    playlist_t *pPlaylist = getIntf()->p_sys->p_playlist;
+    if( pPlaylist == NULL )
+    {
+        return;
+    }
+
+    if( m_playNow )
+    {
+        // Enqueue and play the item
+        playlist_Add( pPlaylist, m_name.c_str(),m_name.c_str(),
+                      PLAYLIST_APPEND | PLAYLIST_GO, PLAYLIST_END );
+    }
+    else
+    {
+        // Enqueue the item only
+        playlist_Add( pPlaylist, m_name.c_str(), m_name.c_str(),
+                      PLAYLIST_APPEND, PLAYLIST_END );
+    }
+}
